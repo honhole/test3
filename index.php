a58d280b64f2e4b468d5adc42ad947ceb15b019a
honhole <honhole@163.com>
2018-09-08 11:26:18 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ggg
+ * Date: 2018-07-17
+ * Time: 10:50
+ */
+namespace app\index\controller;
+use think\Controller;
+class Index extends Common
+{
+    //index
+    public function index()
+    {
+        $case_list = db('product')->order('id desc')->limit(10)->select();
+        $this->assign('case_list',$case_list);
+
+        $news_list = db('news')->order('id desc')->limit(16)->select();
+        $this->assign('news_list',$news_list);
+
+
+
+        return view();
+    }
+}
